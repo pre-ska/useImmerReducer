a184f335005171804ef687684f234d1e69cfa857
wolfwar <wolfwar@outlook.com>
2020-01-07 20:25:42 +0100

    fixed info

--- src/LoginImmer.js
+++ src/LoginImmer.js
@@ -12,6 +12,7 @@ const reducer = (state, action) => {
     }
     case "success":
       state.isLoggedIn = true;
+      state.isLoading = false;
       return;
 
     case "error":
@@ -66,51 +67,59 @@ const LoginImmer = () => {
   };
 
   return (
-    <div className="App">
+    <>
       <div className="login-container">
         {isLoggedIn ? (
           <>
-            <h1>Hello {username}!!!</h1>
+            <img
+              src="https://www.chronicle.com/blogs/linguafranca/files/2017/11/Nothing-to-See-15a34a2fc727c8.jpg"
+              alt="go back"
+            />
             <button onClick={() => dispatch({ type: "logout" })}>
               Log out
             </button>
           </>
         ) : (
-          <form className="form" onSubmit={onSubmit}>
-            {error && <p className="error">{error}</p>}
-            <p>Please Login!</p>
-            <input
-              type="text"
-              placeholder="username"
-              value={username}
-              onChange={e =>
-                dispatch({
-                  type: "field",
-                  field: "username",
-                  value: e.target.value
-                })
-              }
-            />
-            <input
-              type="password"
-              placeholder="password"
-              autoComplete="new-password"
-              value={password}
-              onChange={e =>
-                dispatch({
-                  type: "field",
-                  field: "password",
-                  value: e.target.value
-                })
-              }
-            />
-            <button className="submit" type="submit" disabled={isLoading}>
-              {isLoading ? "Logging in..." : "Log in"}
-            </button>
-          </form>
+          <>
+            <div className="info">
+              <p>username & password: test</p>
+            </div>
+            <form className="form" onSubmit={onSubmit}>
+              {error && <p className="error">{error}</p>}
+              <p>Please Login!</p>
+              <input
+                type="text"
+                placeholder="username"
+                value={username}
+                onChange={e =>
+                  dispatch({
+                    type: "field",
+                    field: "username",
+                    value: e.target.value
+                  })
+                }
+              />
+              <input
+                type="password"
+                placeholder="password"
+                autoComplete="new-password"
+                value={password}
+                onChange={e =>
+                  dispatch({
+                    type: "field",
+                    field: "password",
+                    value: e.target.value
+                  })
+                }
+              />
+              <button className="submit" type="submit" disabled={isLoading}>
+                {isLoading ? "Logging in..." : "Log in"}
+              </button>
+            </form>
+          </>
         )}
       </div>
-    </div>
+    </>
   );
 };
 
